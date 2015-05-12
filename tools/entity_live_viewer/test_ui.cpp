df19cf003fbd57c8225186061cb24feb8b2e4b3e
Luis Ferreira <luisfferreira@outlook.com>
2015-05-12 13:48:54 +0200

    some more attempts at showing the GUI

--- tools/entity_live_viewer/test_ui.cpp
+++ tools/entity_live_viewer/test_ui.cpp
@@ -15,12 +15,6 @@ int main( int argc, char* argv[] )
 
     QApplication app(argc, argv);
 
-    // run directly
-//    QMainWindow *widget = new QMainWindow;
-//    Ui::MainWindow ui;
-//    ui.setupUi(widget);
-//    widget->show();
-
     // turn through another class
     ViewerUI testUI;
     testUI.show();
