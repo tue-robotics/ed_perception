a2e1372496ae969bc811437d03093c3f814b1352
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-10-28 16:55:08 +0100

    feat(size_matcher) Re-implementation of size matcher

--- plugins/size_matcher.cpp
+++ plugins/size_matcher.cpp
@@ -13,18 +13,68 @@
 #include <rgbd/Image.h>
 #include <rgbd/View.h>
 
-// Loading models
-#include <fstream>
+#include <tue/config/write.h>
+#include <tue/config/writer.h>
+#include <tue/config/read.h>
+#include <tue/config/reader.h>
 
-
-// NOTE: currently only looks at hight!
+#include <tue/filesystem/path.h>
 
 // ----------------------------------------------------------------------------------------------------
 
-SizeMatcher::SizeMatcher() :
-    Module("size_matcher"),
-    init_success_(false)
-{}
+namespace
+{
+
+bool calculateSize(const ed::Entity& e, double& width, double& height)
+{
+    ed::MeasurementConstPtr msr = e.lastMeasurement();
+    if (!msr)
+        return false;
+
+    // get depth image
+    const cv::Mat& depth = msr->image()->getDepthImage();
+
+    geo::Vec2 p_min( 1e6,  1e6);
+    geo::Vec2 p_max(-1e6, -1e6);
+
+    bool has_points = false;
+
+    rgbd::View view(*msr->image(), depth.cols);
+    for(ed::ImageMask::const_iterator it = msr->imageMask().begin(depth.cols); it != msr->imageMask().end(); ++it)
+    {
+        const cv::Point2i& p_2d = *it;
+        float d = depth.at<float>(p_2d);
+
+        if (d == 0 || d != d)
+            continue;
+
+        geo::Vec3 p = view.getRasterizer().project2Dto3D(p_2d.x, p_2d.y) * d;
+
+        p_min.x = std::min(p_min.x, p.x);
+        p_min.y = std::min(p_min.y, p.y);
+        p_max.x = std::max(p_max.x, p.x);
+        p_max.y = std::max(p_max.y, p.y);
+
+        has_points = true;
+    }
+
+    if (!has_points)
+        return false;
+
+    width = p_max.x - p_min.x;
+    height = p_max.y - p_min.y;
+
+    return true;
+}
+
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+SizeMatcher::SizeMatcher() : Module("size_matcher")
+{
+    this->registerPropertyServed("type");
+}
 
 // ----------------------------------------------------------------------------------------------------
 
@@ -32,280 +82,129 @@ SizeMatcher::~SizeMatcher(){}
 
 // ----------------------------------------------------------------------------------------------------
 
-
-void SizeMatcher::configure(tue::Configuration config) {
-
-    if (!config.value("size_diff_threshold", size_diff_threshold_, tue::OPTIONAL))
-        std::cout << "[" << module_name_ << "] " << "Parameter 'size_diff_threshold' not found. Using default: " << size_diff_threshold_ << std::endl;
-
-    if (!config.value("small_size_treshold", small_size_treshold_, tue::OPTIONAL))
-        std::cout << "[" << module_name_ << "] " << "Parameter 'small_size_treshold' not found. Using default: " << small_size_treshold_ << std::endl;
-
-    if (!config.value("medium_size_treshold", medium_size_treshold_, tue::OPTIONAL))
-        std::cout << "[" << module_name_ << "] " << "Parameter 'medium_size_treshold' not found. Using default: " << medium_size_treshold_ << std::endl;
-
-    if (!config.value("type_positive_score", type_positive_score_, tue::OPTIONAL))
-        std::cout << "[" << module_name_ << "] " << "Parameter 'type_positive_score' not found. Using default: " << type_positive_score_ << std::endl;
-
-    if (!config.value("type_negative_score", type_negative_score_, tue::OPTIONAL))
-        std::cout << "[" << module_name_ << "] " << "Parameter 'type_negative_score' not found. Using default: " << type_negative_score_ << std::endl;
-
-    if (!config.value("type_unknown_score", type_unknown_score_, tue::OPTIONAL))
-        std::cout << "[" << module_name_ << "] " << "Parameter 'type_unknown_score' not found. Using default: " << type_unknown_score_ << std::endl;
-
-
-    if (models_.empty())
-         std::cout << "[" << module_name_ << "] " << "No information from the models size was loaded!" << std::endl;
-    else
-        std::cout << "[" << module_name_ << "] " << "Loaded size information for " << models_.size() << " different models" << std::endl;
-
-    init_success_ = true;
-
-    std::cout << "[" << module_name_ << "] " << "Ready!"<< std::endl;
-}
-
-// ----------------------------------------------------------------------------------------------------
-
-void SizeMatcher::loadConfig(const std::string& config_path) {
-
-    module_name_ = "size_matcher";
-
-    // default values in case configure(...) is not called!
-    size_diff_threshold_ = 0.8;
-    small_size_treshold_ = 0.5;
-    medium_size_treshold_ = 0.7;
-    type_positive_score_ = 0.9;
-    type_negative_score_ = 0.4;
-    type_unknown_score_ = 0.05;
-}
-
-// ----------------------------------------------------------------------------------------------------
-
-void SizeMatcher::loadModel(const std::string& model_name, const std::string& model_path)
+void SizeMatcher::classify(const ed::Entity& e, const std::string& property, const ed::perception::CategoricalDistribution& prior,
+              ed::perception::ClassificationOutput& output) const
 {
-
-    std::string models_folder = model_path.substr(0, model_path.find_last_of("/") - 1); // remove last slash
-    models_folder = models_folder.substr(0, models_folder.find_last_of("/"));   // remove size_matcher from path
-
-    std::string path = models_folder + "/models/" + model_name +  "/" +  model_name + ".yml";
-
-    if (loadLearnedModel(path, model_name)){
-//        std::cout << "[" << module_name_ << "] " << "Loaded sizes for " << model_name << std::endl;
-    }
-    else{
-//        std::cout << "[" << module_name_ << "] " << "Couldn not load sizes for " << path << "!" << std::endl;
-    }
-}
-
-
-// ----------------------------------------------------------------------------------------------------
-
-void SizeMatcher::process(const ed::perception::WorkerInput& input, ed::perception::WorkerOutput& output) const
-{
-    const ed::EntityConstPtr& e = input.entity;
-    tue::Configuration& result = output.data;
-
-    ed::ErrorContext errc("Processing entity in SizeMatcher");
-
-    if (!init_success_)
+    if (property != "type")
         return;
 
-    const ed::ConvexHull& conv_hull = e->convexHull();
-
-    // if there is no convex hull available, cancel classification
-    if (conv_hull.points.empty())
+    double width, height;
+    if (!calculateSize(e, width, height))
         return;
 
-    std::map<std::string, double> hypothesis;
-
-    // initialize size class
-    bool size_small = false;
-    bool size_medium = false;
-    bool size_big = false;
-
-    double object_height = conv_hull.height();
-    double object_area = conv_hull.area;
-
-    if (object_area <= 0)
-        std::cout << "[" << module_name_ << "] " << "Bad area value for the entity (area = " << object_area << ")" << std::endl;
-
-    // set object size class
-    if (object_area < small_size_treshold_){
-        size_small = true;
-    }else if (small_size_treshold_ < object_area && object_area < medium_size_treshold_){
-        size_medium = true;
-    }else if (medium_size_treshold_ < object_area){
-        size_big = true;
-    }else
-        std::cout << "[" << module_name_ << "] " << "Could not set a size threshold!" << std::endl;
-
-    // compare object size to loaded models
-    for(std::map<std::string, std::vector<ObjectSize> >::const_iterator it = models_.begin(); it != models_.end(); ++it)
+    for(std::map<std::string, SizeModel>::const_iterator it = models_.begin(); it != models_.end(); ++it)
     {
-        const std::string& label = it->first;
-        const std::vector<ObjectSize>& sizes = it->second;
+        const std::string& model_name = it->first;
+        const SizeModel& model = it->second;
 
-        double best_score = 0;
-        for(std::vector<ObjectSize>::const_iterator it_size = sizes.begin(); it_size != sizes.end(); ++it_size)
+        if (model.width_min < width && width < model.width_max
+                && model.height_min < height && height < model.height_max)
         {
-            const ObjectSize& model_size = *it_size;
-
-            double h_ratio = object_height / model_size.height;
-            double a_ratio = object_area / model_size.area;
-
-            double h_score;
-            if (h_ratio > 1.5)
-                h_score = 0;
-            else
-            {
-                if (h_ratio > 1)
-                    h_ratio = 1 / h_ratio;
-
-                h_score = h_ratio;
-            }
-
-            double a_score;
-            if (a_ratio > 1.5)
-                a_score = 0;
-            else
-            {
-                if (a_ratio > 1)
-                    a_ratio = 1 / a_ratio;
-
-                a_score = a_ratio;
-            }
-
-            double final_score;
-
-            // temporary bug fix when area = 0
-            if (a_score == 0)
-                final_score = h_score;
-            else
-                final_score = h_score * a_score;
-
-            // update best score
-            best_score = std::max(best_score, final_score);
+            double score = 1.0 / (model.width_max - model.width_min) + 1.0 / (model.height_max - model.height_min);
+//            double score = 1;
+            output.likelihood.setScore(model_name, score);
         }
-
-        hypothesis[label] = 0.5 * best_score; // TODO: magic number
-    }
-
-    // ----------------------- ASSERT RESULTS -----------------------
-
-    // create group if it doesnt exist
-    if (!result.readGroup("perception_result", tue::OPTIONAL))
-    {
-        result.writeGroup("perception_result");
-    }
-
-    result.writeGroup("size_matcher");
-
-    result.writeGroup("size");
-    result.setValue("height", object_height);
-    result.setValue("area", object_area);
-    result.endGroup();
-
-    // assert hypothesis
-    if (!hypothesis.empty()){
-        for (std::map<std::string, double>::const_iterator it = hypothesis.begin(); it != hypothesis.end(); ++it){
-//                output.type_update.setScore(it->first, std::max(it->second/2, type_negative_score_));
-
-            // if the best score is above 0 assert that, otherwise assert a negative score
-            if (it->second > 0)
-                output.type_update.setScore(it->first, it->second);
-            else
-                output.type_update.setScore(it->first, type_negative_score_);
-
-            // std::cout << "[" << module_name_ << "] " << "Asserting: " << it->first << ": " << it->second << std::endl;
-        }
-    }
-
-    // Set labels and scores
-    if (size_small)
-    {
-        result.setValue("label", "small_size");
-
-        // if its small, for sure its not a person
-        output.type_update.setScore("human", type_negative_score_);
-//        output.type_update.setScore("crowd", type_negative_score_);
-
-    }else if (size_medium){
-        result.setValue("label", "medium_size");
-
-//        output.type_update.setScore("human", type_positive_score_);
-//        output.type_update.setScore("crowd", type_positive_score_);
-
-    }else if (size_big){
-        result.setValue("label", "large_size");
-
-        // output.type_update.setScore("human", type_positive_score_);
-//        output.type_update.setScore("crowd", 0.5);
-
-//        output.type_update.setScore("human", type_positive_score_);
-//        output.type_update.setScore("crowd", type_positive_score_);
-    }else
-        std::cout << "[" << module_name_ << "] " << "Could not get size class!" << std::endl;
-
-    output.type_update.setUnknownScore(type_unknown_score_);
-
-    result.endGroup();  // close size_matcher group
-    result.endGroup();  // close perception_result group
-}
-
-
-// ----------------------------------------------------------------------------------------------------
-
-
-bool SizeMatcher::loadLearnedModel(std::string path, std::string model_name){
-    if (path.empty())
-    {
-        std::cout << "[" << module_name_ << "] " << "Empty path!" << path << std::endl;
-        return false;
-    }
-
-    tue::Configuration conf;
-    double height;
-    double area;
-    std::vector<ObjectSize> model_sizes;
-
-    if (!conf.loadFromYAMLFile(path))
-    {
-//        std::cout << "Could not load " << path << std::endl;
-        return false;
-    }
-
-    if (!conf.readGroup("model"))
-    {
-        std::cout << "[" << module_name_ << "] " << "Could not find 'model' group" << std::endl;
-        return false;
-    }
-
-    if (conf.readArray("size")) // read Size arary
-    {
-        while(conf.nextArrayItem())
-        {
-            if (conf.value("height", height, tue::OPTIONAL) && conf.value("area", area, tue::OPTIONAL))  // read height and area
-            {
-                ObjectSize obj_sz(0, height, area);
-                model_sizes.push_back(obj_sz);
-            }
-            else
-                std::cout << "[" << module_name_ << "] " << "Could not find 'height' and 'area' values" << std::endl;
-        }
-
-        if (!model_sizes.empty())  // save sizes to map
-            models_[model_name] = model_sizes;
         else
-            std::cout << "[" << module_name_ << "] " << "Could not read any sizes" << std::endl;
+        {
+            output.likelihood.setScore(model_name, 0);
+        }
+    }
 
-        conf.endArray();    // close Size array
-    }else
-        std::cout << "[" << module_name_ << "] " << "Could not find 'size' group" << std::endl;
-
-    conf.endGroup();  // close Model group
-
-    return true;
+    // Represent data (for debugging)
+    tue::Configuration& result = output.data;
+    result.setValue("width", width);
+    result.setValue("height", height);
 }
+
+// ----------------------------------------------------------------------------------------------------
+
+void SizeMatcher::addTrainingInstance(const ed::Entity& e, const std::string& property, const std::string& value)
+{
+    if (property != "type")
+        return;
+
+    double width, height;
+    if (!calculateSize(e, width, height))
+        return;
+
+    std::map<std::string, SizeModel>::iterator it = models_.find(value);
+    if (it == models_.end())
+    {
+        // No other training instances for this type
+        SizeModel& model = models_[value];
+        model.width_min = width;
+        model.width_max = width;
+        model.height_min = height;
+        model.height_max = height;
+    }
+    else
+    {
+        SizeModel& model = it->second;
+        model.width_min = std::min(model.width_min, width);
+        model.width_max = std::max(model.width_max, width);
+        model.height_min = std::min(model.height_min, height);
+        model.height_max = std::max(model.height_max, height);
+    }
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+void SizeMatcher::loadRecognitionData(const std::string& path_str)
+{
+    tue::filesystem::Path path(path_str + "/models.yaml");
+    if (!path.exists())
+        return;
+
+    tue::config::DataPointer data = tue::config::fromFile(path.string());
+
+    tue::config::Reader r(data);
+
+    if (!r.readArray("models"))
+        return;
+
+    while(r.nextArrayItem())
+    {
+        std::string model_name;
+        if (!r.value("name", model_name))
+            continue;
+
+        SizeModel& model = models_[model_name];
+
+        r.value("width_min", model.width_min);
+        r.value("width_max", model.width_max);
+        r.value("height_min", model.height_min);
+        r.value("height_max", model.height_max);
+    }
+
+    r.endArray(); // models
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+void SizeMatcher::saveRecognitionData(const std::string& path) const
+{
+    tue::config::Writer w;
+
+    w.writeArray("models");
+    for(std::map<std::string, SizeModel>::const_iterator it = models_.begin(); it != models_.end(); ++it)
+    {
+        const std::string& model_name = it->first;
+        const SizeModel& model = it->second;
+
+        w.addArrayItem();
+        w.setValue("name", model_name);
+        w.setValue("width_min", model.width_min);
+        w.setValue("width_max", model.width_max);
+        w.setValue("height_min", model.height_min);
+        w.setValue("height_max", model.height_max);
+        w.endArrayItem();
+    }
+    w.endArray();
+
+    tue::config::toFile(path + "/models.yaml", w.data(), tue::config::YAML, 4);
+}
+
+// ----------------------------------------------------------------------------------------------------
 
 ED_REGISTER_PERCEPTION_MODULE(SizeMatcher)
 
--- plugins/size_matcher.h
+++ plugins/size_matcher.h
@@ -9,22 +9,17 @@
 
 #include <ed/perception/module.h>
 
-struct ObjectSize
+// ----------------------------------------------------------------------------------------------------
+
+struct SizeModel
 {
-    double height;
-    double width;
-    double area;
-
-    ObjectSize(){}
-
-    ObjectSize(double width_, double height_, double area_)
-        : width(width_), height(height_), area(area_){}
+    double width_min;
+    double width_max;
+    double height_min;
+    double height_max;
 };
 
-
-// ##################################################################################
-// ##################################################################################
-
+// ----------------------------------------------------------------------------------------------------
 
 class SizeMatcher : public ed::perception::Module
 {
@@ -35,48 +30,24 @@ public:
 
     virtual ~SizeMatcher();
 
-    void loadModel(const std::string& model_name, const std::string& model_path);
-
-    void loadConfig(const std::string& config_path);
-
-    void process(const ed::perception::WorkerInput& input, ed::perception::WorkerOutput& output) const;
-
-    void configure(tue::Configuration config);
+    void process(const ed::perception::WorkerInput& input, ed::perception::WorkerOutput& output) const {}
 
 
     // New interface
 
     void classify(const ed::Entity& e, const std::string& property, const ed::perception::CategoricalDistribution& prior,
-                  ed::perception::ClassificationOutput& output) const {}
+                  ed::perception::ClassificationOutput& output) const;
 
-    void addTrainingInstance(const ed::Entity& e, const std::string& property, const std::string& value) {}
+    void addTrainingInstance(const ed::Entity& e, const std::string& property, const std::string& value);
 
-    void train() {}
+    void loadRecognitionData(const std::string& path);
 
-    void loadRecognitionData(const std::string& path) {}
-
-    void saveRecognitionData(const std::string& path) const {}
+    void saveRecognitionData(const std::string& path) const;
 
 private:
 
-    // VARIABLES
+    std::map<std::string, SizeModel> models_;
 
-    bool init_success_;
-
-    double small_size_treshold_;
-    double medium_size_treshold_;
-    double size_diff_threshold_;
-
-    std::map<std::string, std::vector<ObjectSize> > models_;
-    std::string	module_name_;    /*!< Name of the module, for output */
-
-    double type_positive_score_;
-    double type_negative_score_;
-    double type_unknown_score_;
-
-    // FUNCTIONS
-
-    bool loadLearnedModel(std::string path, std::string model_name);
 };
 
 #endif
