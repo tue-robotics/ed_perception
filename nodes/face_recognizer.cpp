6af7aa79c028fe6cec0762dcd2f411dfb53e1340
Sjoerd van den Dries <sjoerdvddries@gmail.com>
2016-02-02 22:05:16 +0100

    feature(face_recognition): stand-alone node can detect faces

--- nodes/face_recognizer.cpp
+++ nodes/face_recognizer.cpp
@@ -1,5 +1,8 @@
 #include "face_recognizer.h"
 
+#include <ros/package.h>
+#include <opencv2/imgproc/imgproc.hpp>
+
 // ----------------------------------------------------------------------------------------------------
 
 FaceRecognizer::FaceRecognizer()
@@ -14,15 +17,58 @@ FaceRecognizer::~FaceRecognizer()
 
 // ----------------------------------------------------------------------------------------------------
 
+bool FaceRecognizer::initialize()
+{
+    // Load face classifier model from 'ed_perception_models' package
+    std::string data_path = ros::package::getPath("ed_perception_models") + "/general/face/";
+    face_detector_.load(data_path + "haarcascade_frontalface_alt_tree.xml");
+}
+
+// ----------------------------------------------------------------------------------------------------
+
 bool FaceRecognizer::train(const cv::Mat& rgb_image, const std::string& name)
 {
+    // - - - - - - - - - - - - - - - - - - - - -
+    // TODO: replace with training face
+
+    // For now: simply remember the name
+    names_.insert(name);
+
+    // - - - - - - - - - - - - - - - - - - - - -
+
     return false;
 }
 
 // ----------------------------------------------------------------------------------------------------
 
-bool FaceRecognizer::find(const cv::Mat& rgb_image, const std::string& name, cv::Rect* roi) const
+void FaceRecognizer::find(const cv::Mat& rgb_image, std::vector<FaceRecognitionResult>& detections)
 {
-    return false;
+    // Convert image to grayscale
+    cv::Mat image_grayscale;
+    cv::cvtColor(rgb_image, image_grayscale, CV_BGR2GRAY);
+
+    // Equalize histogram
+    cv::equalizeHist( image_grayscale, image_grayscale );
+
+    // Detect faces. Returns region of interests of the faces
+    std::vector<cv::Rect> face_rois;
+    face_detector_.detectMultiScale(image_grayscale, face_rois, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
+
+    // For each roi: try to estimate which person the face belongs to, the age, the gender and the body pose
+    detections.resize(face_rois.size());
+    for(unsigned int i = 0; i < face_rois.size(); ++i)
+    {
+        FaceRecognitionResult& det = detections[i];
+        det.rgb_roi = face_rois[i];
+
+        // - - - - - - - - - - - - - - - - - - - - -
+        // TODO: replace with face recognition
+
+        // For now: simple dummy which always returns the first learned name
+        if (!names_.empty())
+            det.name = *names_.begin();
+
+        // - - - - - - - - - - - - - - - - - - - - -
+    }
 }
 
