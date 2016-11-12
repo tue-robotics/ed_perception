bcdecb916da2f822efb7018d9ee5110ff7f196da
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2016-11-12 10:38:11 +0100

    Created backwards compatibility with indigo

--- plugins/face_detector.h
+++ plugins/face_detector.h
@@ -11,9 +11,9 @@
 
 // OpenCV includes
 #include <opencv/cv.h>
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
 
 class FaceDetector : public ed::perception::Module
 {
